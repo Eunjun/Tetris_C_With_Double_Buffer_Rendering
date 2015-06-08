47c6078fc50e8b4ffe7e8723d397c15e328f2ca2
장은준 <jangej1031@naver.com>
2015-06-08 09:44:59 +0900

    common.h
    //파일명 - common.h
    //설명 - 일반적인 헤더파일과 상수를 정의
    //작성자 - 장은준
    //작성일자 - 2015-04-17

---
+++ common.h
@@ -0,0 +1,32 @@
+//파일명 - common.h
+//설명 - 일반적인 헤더파일과 상수를 정의
+//작성자 - 장은준
+//작성일자 - 2015-04-17
+
+#ifndef __COMMON_H_
+#define __COMMON_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+#include <Windows.h>
+
+#define BW		10
+#define BH		20
+
+//방향키 오른쪽
+#define RIGHT	77
+//방향키 왼쪽
+#define LEFT	75
+//방향키 위쪽
+#define UP		72
+//방향키 아래쪽
+#define DOWN	80
+//스페이스 바
+#define SPACE	32
+//엔터
+#define ENTER	13
+
+#endif
